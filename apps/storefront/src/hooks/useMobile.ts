bc215ca69f964852ea95334f95cdb81804f527e4
aton.xia <aton.xia@bundleb2b.net>
2023-01-31 16:19:52 +0800

    feat: update mobile view

--- apps/storefront/src/hooks/useMobile.ts
+++ apps/storefront/src/hooks/useMobile.ts
@@ -6,15 +6,26 @@ import {
 const useMobile = () => {
   const [isMobile, setIsMobile] = useState<boolean>(false)
 
-  const ua = navigator.userAgent.toLowerCase()
-  const agents = ['iphone', 'ipad', 'ipod', 'android', 'windows phone'] // All fields that may be mobile devices
+  // const ua = navigator.userAgent.toLowerCase()
+  // const agents = ['iphone', 'ipad', 'ipod', 'android', 'windows phone'] // All fields that may be mobile devices
 
   useEffect(() => {
-    agents.forEach((item: string) => {
-      if (ua.indexOf(item) !== -1) {
-        setIsMobile(true)
-      }
-    })
+    // agents.forEach((item: string) => {
+    //   if (ua.indexOf(item) !== -1) {
+    //     setIsMobile(true)
+    //   }
+    // })
+    const resize = () => {
+      setIsMobile(document.body.clientWidth <= 750)
+    }
+
+    resize()
+
+    window.addEventListener('resize', resize)
+
+    return () => {
+      window.removeEventListener('resize', resize)
+    }
   }, [])
 
   return [isMobile]
