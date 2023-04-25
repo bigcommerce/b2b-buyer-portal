99aada563070a91c8d4a8f44f2f2ca59674b56f1
Carl <carl.liu@bundleb2b.net>
2023-04-25 17:48:10 +0800

    fix: currency bug

--- apps/storefront/src/utils/b3CurrencyFormat.ts
+++ apps/storefront/src/utils/b3CurrencyFormat.ts
@@ -1,8 +1,8 @@
 import getDefaultCurrencyInfo from './currencyUtils'
 
 interface MoneyFormat {
-  token_location: 'left' | 'right'
-  token: string
+  currency_location: 'left' | 'right'
+  currency_token: string
   decimal_token: string
   decimal_places: number
   thousands_token: string
@@ -10,31 +10,31 @@ interface MoneyFormat {
 }
 
 const currencyFormat = (price: string | number, showCurrencyToken = true) => {
-  const {
-    token_location: tokenLocation = 'left',
-    token: currencyToken = '$',
-    decimal_token: decimalToken = '.',
-    decimal_places: decimalPlaces = 2,
-    thousands_token: thousandsToken = ',',
-    currency_exchange_rate: currencyExchangeRate = '1.0000000000',
-  }: MoneyFormat = getDefaultCurrencyInfo() || {}
+  const currentCurrency = getDefaultCurrencyInfo()
+
+  const moneyFormat: MoneyFormat = {
+    currency_location: currentCurrency?.token_location || 'left',
+    currency_token: currentCurrency?.token || '$',
+    decimal_token: currentCurrency?.decimal_token || '.',
+    decimal_places: currentCurrency?.decimal_places || 2,
+    thousands_token: currentCurrency?.thousands_token || ',',
+    currency_exchange_rate:
+      currentCurrency?.currency_exchange_rate || '1.0000000000',
+  }
 
   try {
-    const priceValue = +price * +currencyExchangeRate
-    const [integerPart, decimalPart = ''] = priceValue
-      .toFixed(decimalPlaces)
-      .split('.')
-    const integerPartWithSeparator = integerPart.replace(
-      /\B(?=(\d{3})+(?!\d\.))/g,
-      thousandsToken
-    )
-    const priceStr = `${integerPartWithSeparator}${
-      decimalPart ? `${decimalToken}${decimalPart}` : ''
-    }`
-
-    return tokenLocation === 'left'
-      ? `${showCurrencyToken ? currencyToken : ''}${priceStr}`
-      : `${priceStr}${showCurrencyToken ? currencyToken : ''}`
+    const newPrice = `${(+price * +moneyFormat.currency_exchange_rate)
+      .toFixed(moneyFormat.decimal_places)
+      .replace(/\B(?=(\d{3})+(?!\d))/g, moneyFormat.thousands_token)}`
+    const priceStr =
+      moneyFormat.currency_location === 'left'
+        ? `${showCurrencyToken ? moneyFormat.currency_token : ''}${newPrice}`
+        : `${newPrice}${showCurrencyToken ? moneyFormat.currency_token : ''}`
+    const location = priceStr.lastIndexOf('.')
+    const newPriceStr = `${priceStr.slice(0, location)}${
+      moneyFormat.decimal_token
+    }${priceStr.slice(location + 1)}`
+    return newPriceStr
   } catch (e) {
     console.error(e)
     return ''
