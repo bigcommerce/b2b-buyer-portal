1044c318c330b7bb1177ee4f9842eddb7f717b4c
Brian.Jiang2021 <brian.jiang@bundleb2b.net>
2023-05-23 14:49:31 +0800

    fix: quick order issues

--- apps/storefront/src/pages/quickorder/Quickorder.tsx
+++ apps/storefront/src/pages/quickorder/Quickorder.tsx
@@ -44,6 +44,10 @@ function Quickorder() {
             xs={isMobile ? 12 : 8}
             sx={{
               backgroundColor: '#ffffff',
+              boxShadow:
+                '0px 2px 1px -1px rgba(0, 0, 0, 0.2), 0px 1px 1px rgba(0, 0, 0, 0.14), 0px 1px 3px rgba(0, 0, 0, 0.12)',
+              borderRadius: '4px',
+              pr: '16px',
             }}
           >
             <QuickorderTable
