c54bb6419a76ad4779d724f39254fdbec51403ae
Carl <carl.liu@bundleb2b.net>
2023-06-14 16:46:21 +0800

    fix: product with required modifer can upload by csv

--- apps/storefront/src/pages/quote/components/AddToQuote.tsx
+++ apps/storefront/src/pages/quote/components/AddToQuote.tsx
@@ -298,6 +298,7 @@ export default function AddToQuote(props: AddToListProps) {
             setIsOpen={setIsOpenBulkLoadCSV}
             handleAddToList={handleCSVAddToList}
             isLoading={isLoading}
+            withModifiers
           />
         </B3CollapseContainer>
       </CardContent>
