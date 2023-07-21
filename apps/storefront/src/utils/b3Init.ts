b49d39813bc3177be5e7ae1fb22283a2062662fd
Carl <carl.liu@bundleb2b.net>
2023-07-21 13:42:10 +0800

    fix: quick order button display issues

--- apps/storefront/src/utils/b3Init.ts
+++ apps/storefront/src/utils/b3Init.ts
@@ -32,29 +32,51 @@ export const getQuoteEnabled = (
   let productEnabled = '1'
   let cartEnabled = '1'
 
+  let slGuestEnabled = true
+  let slBcUserEnabled = true
+  let slB2bUserEnabled = true
+
   const quoteEnabled = storefrontConfig.quotes || false
 
   const shoppingListEnabled = storefrontConfig.shoppingLists
   const registerEnabled = storefrontConfig.tradeProfessionalApplication
 
   quoteConfig.forEach((config) => {
-    if (config.key === 'quote_customer') {
-      customerEnabled = config.value
-    }
-    if (config.key === 'quote_for_guest') {
-      guestEnabled = config.value
-    }
-    if (config.key === 'quote_for_individual_customer') {
-      bcUserEnabled = config.value
-    }
-    if (config.key === 'quote_for_b2b') {
-      b2bUserEnabled = config.value
-    }
-    if (config.key === 'quote_on_product_page') {
-      productEnabled = config.value
-    }
-    if (config.key === 'quote_on_cart_page') {
-      cartEnabled = config.value
+    switch (config.key) {
+      case 'quote_customer':
+        customerEnabled = config.value
+        break
+
+      case 'quote_for_guest':
+        guestEnabled = config.value
+        break
+
+      case 'quote_for_individual_customer':
+        bcUserEnabled = config.value
+        break
+
+      case 'quote_for_b2b':
+        b2bUserEnabled = config.value
+        break
+
+      case 'quote_on_product_page':
+        productEnabled = config.value
+        break
+
+      case 'quote_on_cart_page':
+        cartEnabled = config.value
+        break
+
+      case 'shopping_list_on_product_page':
+        if (config?.extraFields) {
+          slGuestEnabled = config?.extraFields?.guest || false
+          slBcUserEnabled = config?.extraFields?.b2c || false
+          slB2bUserEnabled = config?.extraFields?.b2b || false
+        }
+        break
+
+      default:
+        break
     }
   })
 
@@ -62,28 +84,33 @@ export const getQuoteEnabled = (
     quoteEnabled && customerEnabled === '1' && productEnabled === '1'
   let cartQuoteEnabled =
     quoteEnabled && customerEnabled === '1' && cartEnabled === '1'
+  let productShoppingListEnabled = shoppingListEnabled
 
   if (`${role}` === '100') {
     // guest
     productQuoteEnabled = productQuoteEnabled && guestEnabled === '1'
     cartQuoteEnabled = cartQuoteEnabled && guestEnabled === '1'
+    productShoppingListEnabled = shoppingListEnabled && slGuestEnabled
   } else if (isB2BUser) {
     productQuoteEnabled = productQuoteEnabled && b2bUserEnabled === '1'
     cartQuoteEnabled = cartQuoteEnabled && b2bUserEnabled === '1'
+    productShoppingListEnabled = shoppingListEnabled && slB2bUserEnabled
     if (`${role}` === '3' && !isAgenting) {
       productQuoteEnabled = false
       cartQuoteEnabled = false
+      productShoppingListEnabled = false
     }
   } else if (!isB2BUser) {
     // BCUser
     productQuoteEnabled = productQuoteEnabled && bcUserEnabled === '1'
     cartQuoteEnabled = cartQuoteEnabled && bcUserEnabled === '1'
+    productShoppingListEnabled = shoppingListEnabled && slBcUserEnabled
   }
 
   return {
     productQuoteEnabled,
     cartQuoteEnabled,
-    shoppingListEnabled,
+    shoppingListEnabled: productShoppingListEnabled,
     registerEnabled,
   }
 }
