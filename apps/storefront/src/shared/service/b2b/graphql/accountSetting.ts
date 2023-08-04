66a5557f785ee086cd29c2c53f34149adbae5d39
Carl <carl.liu@bundleb2b.net>
2023-08-04 16:37:03 +0800

    fix: super admin account page issues

--- apps/storefront/src/shared/service/b2b/graphql/accountSetting.ts
+++ apps/storefront/src/shared/service/b2b/graphql/accountSetting.ts
@@ -62,7 +62,7 @@ export const updateB2BAccountSettings = (
 export const updateBCAccountSettings = (
   data: CustomFieldItems
 ): CustomFieldItems =>
-  B3Request.graphqlB2B({
+  B3Request.graphqlB2BWithBCCustomerToken({
     query: updateCustomerAccountSettings(data),
   })
 
@@ -74,6 +74,6 @@ export const getB2BAccountSettings = (
   })
 
 export const getBCAccountSettings = (): CustomFieldItems =>
-  B3Request.graphqlB2B({
+  B3Request.graphqlB2BWithBCCustomerToken({
     query: customerAccountSettings(),
   })
