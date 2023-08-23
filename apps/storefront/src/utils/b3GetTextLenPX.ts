cb9e7ba9cb87f2f6b0bd96886b7ecca1ed60064a
Brian.Jiang2021 <brian.jiang@bundleb2b.net>
2023-08-23 13:57:03 +0800

    fix: b2Tob2b page title wrapping

--- apps/storefront/src/utils/b3GetTextLenPX.ts
+++ apps/storefront/src/utils/b3GetTextLenPX.ts
@@ -15,7 +15,7 @@ const getLineNumber = (text: string, fontSize = 14) => {
 
   // pc padding: 12.8 + 12.8 + 8 + 8 + 12 + 12, origin width: 537px
   // mobile padding: (12.8 + 16 + 8 + 12) * 2, origin width: screenWidth, body margin: 16
-  const pcLen = 470
+  const pcLen = 500
   const mobileLen = screenWidth + 16 - 98
 
   const screenLen = isMobile ? mobileLen : pcLen
