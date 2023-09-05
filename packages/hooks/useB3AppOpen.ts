d693fc526bd786d3735c5ac7030c2546a6e9fbbf
BrianJiang2021 <80307788+BrianJiang2021@users.noreply.github.com>
2023-09-05 13:39:01 +0800

    feat: add theme compatible (#717)
    * feat: add theme compatible
    https://bigc-b2b.atlassian.net/browse/BUN-1407

--- packages/hooks/useB3AppOpen.ts
+++ packages/hooks/useB3AppOpen.ts
@@ -7,6 +7,11 @@ import useMutationObservable from './useMutationObservable'
 //   href: string
 //   isRegisterArrInclude: boolean
 // }
+interface ChildNodeListProps extends ChildNode {
+  href?: string
+  localName?: string
+}
+
 export interface OpenPageState {
   isOpen: boolean
   openUrl?: string
@@ -47,7 +52,29 @@ export const useB3AppOpen = (initOpenState: OpenPageState) => {
           e.preventDefault()
           e.stopPropagation()
           const isRegisterArrInclude = registerArr.includes(e.target)
-          const href = (e.target as HTMLAnchorElement)?.href || '/orders'
+          const tagHref = (e.target as HTMLAnchorElement)?.href
+          let href = tagHref || '/orders'
+          if (!tagHref) {
+            const parentNode = (e.target as HTMLAnchorElement)?.parentNode
+            const parentNodeOrigin = (e.target as HTMLAnchorElement)?.parentNode
+              ?.parentNode
+            const parentHref =
+              (parentNodeOrigin as HTMLAnchorElement)?.href ||
+              (parentNode as HTMLAnchorElement)?.href
+            if (parentHref) {
+              href = parentHref || '/orders'
+            } else {
+              const childNodeList = (e.target as HTMLAnchorElement)?.childNodes
+              if (childNodeList.length > 0) {
+                childNodeList.forEach((node: ChildNodeListProps) => {
+                  const nodeHref = node?.href
+                  if (nodeHref && node.localName === 'a') {
+                    href = nodeHref || '/orders'
+                  }
+                })
+              }
+            }
+          }
           if (initOpenState?.handleEnterClick) {
             initOpenState.handleEnterClick(href, isRegisterArrInclude)
           }
