e33c377f0757397144b5dc50216c4578207274c1
kris-liu-smile <kris.liu@bundleb2b.net>
2023-09-05 09:40:10 +0800

    fix: support headless login

--- apps/storefront/src/utils/loginInfo.ts
+++ apps/storefront/src/utils/loginInfo.ts
@@ -6,7 +6,7 @@ import {
   getBCGraphqlToken,
   getUserCompany,
 } from '@/shared/service/b2b'
-import { getBcCurrentJWT, getCustomerInfo } from '@/shared/service/bc'
+import { getBcCurrentJWT } from '@/shared/service/bc'
 import { B3LStorage, B3SStorage, storeHash } from '@/utils'
 
 const { VITE_B2B_CLIENT_ID, VITE_LOCAL_DEBUG } = import.meta.env
@@ -82,9 +82,9 @@ export const getCurrentStoreInfo = (
 
   const { origin } = window.location
   const cleanedOrigin = origin.replace('-1.', '.')
-  const storeItem = enabledStores.find((item: ChannelIdProps) => item.urls
-      .map((url) => url.replace('-1.', '.'))
-      .includes(cleanedOrigin))
+  const storeItem = enabledStores.find((item: ChannelIdProps) =>
+    item.urls.map((url) => url.replace('-1.', '.')).includes(cleanedOrigin)
+  )
 
   return storeItem || store
 }
@@ -340,11 +340,11 @@ export const getCurrentCustomerInfo = async (
   b2bToken?: string
 ) => {
   try {
-    const {
-      data: { customer },
-    } = await getCustomerInfo()
+    const loginCustomer = B3SStorage.get('loginCustomer')
 
-    if (!customer) return undefined
+    if (!loginCustomer) {
+      return undefined
+    }
 
     const {
       entityId: customerId = '',
@@ -353,7 +353,7 @@ export const getCurrentCustomerInfo = async (
       lastName,
       email: emailAddress = '',
       customerGroupId,
-    } = customer
+    } = loginCustomer
 
     const companyUserInfo = await getCompanyUserInfo(
       emailAddress,
