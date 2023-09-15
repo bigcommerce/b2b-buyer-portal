4c90a402a0a2a4f5c207434b4be45472709831cb
kris-liu-smile <kris.liu@bundleb2b.net>
2023-09-15 10:29:39 +0800

    fix: shoppingList footer permission repair https://bigc-b2b.atlassian.net/jira/software/c/projects/BUN/issues/BUN-1354

--- apps/storefront/src/pages/shoppingListDetails/components/ShoppingDetailFooter.tsx
+++ apps/storefront/src/pages/shoppingListDetails/components/ShoppingDetailFooter.tsx
@@ -459,22 +459,48 @@ function ShoppingDetailFooter(props: ShoppingDetailFooterProps) {
     }
   }
 
-  const buttonList = [
-    {
-      name: allowJuniorPlaceOrder
-        ? b3Lang('shoppingList.footer.proceedToCheckout')
-        : b3Lang('shoppingList.footer.addToCart'),
+  const buttons = {
+    adSelectedToCart: {
+      name: b3Lang('shoppingList.footer.addToCart'),
       key: 'add-selected-to-cart',
       handleClick: handleAddProductsToCart,
       isDisabled: false,
     },
-    {
+    proceedToCheckout: {
+      name: b3Lang('shoppingList.footer.proceedToCheckout'),
+      key: 'add-select-to-checkout',
+      handleClick: handleAddProductsToCart,
+      isDisabled: false,
+    },
+    addSelectedToQuote: {
       name: b3Lang('shoppingList.footer.addToQuote'),
       key: 'add-selected-to-quote',
       handleClick: handleAddSelectedToQuote,
-      isDisabled: !productQuoteEnabled,
+      isDisabled: false,
     },
-  ]
+  }
+
+  const allowButtonList = () => {
+    if (!(shoppingListInfo?.status === 0 || !isB2BUser)) return []
+
+    if (role === 2) {
+      if (allowJuniorPlaceOrder && productQuoteEnabled) {
+        return [buttons.proceedToCheckout, buttons.addSelectedToQuote]
+      }
+
+      if (allowJuniorPlaceOrder) return [buttons.proceedToCheckout]
+      if (productQuoteEnabled) {
+        return [buttons.adSelectedToCart, buttons.addSelectedToQuote]
+      }
+      return []
+    }
+
+    return productQuoteEnabled
+      ? [buttons.adSelectedToCart, buttons.addSelectedToQuote]
+      : [buttons.adSelectedToCart]
+  }
+
+  const buttonList = allowButtonList()
 
   return (
     <Grid
@@ -584,8 +610,7 @@ function ShoppingDetailFooter(props: ShoppingDetailFooterProps) {
                   />
                 </CustomButton>
               )}
-              {((role !== 2 && shoppingListInfo?.status === 0) ||
-                !isB2BUser) && (
+              {buttonList.length && (
                 <Box
                   sx={{
                     display: 'flex',
@@ -595,42 +620,54 @@ function ShoppingDetailFooter(props: ShoppingDetailFooterProps) {
                     width: isMobile ? '100%' : 'auto',
                   }}
                 >
-                  <CustomButton
-                    variant="contained"
-                    onClick={handleOpenBtnList}
-                    sx={{
-                      marginRight: isMobile ? '1rem' : 0,
-                      width: isMobile ? '100%' : 'auto',
-                    }}
-                    endIcon={<ArrowDropDown />}
-                  >
-                    {b3Lang('shoppingList.footer.addSelectedTo')}
-                  </CustomButton>
-                  <Menu
-                    id="basic-menu"
-                    anchorEl={anchorEl}
-                    open={open}
-                    onClose={handleClose}
-                    MenuListProps={{
-                      'aria-labelledby': 'basic-button',
-                    }}
-                  >
-                    {buttonList.length > 0 &&
-                      buttonList.map((button) => {
-                        if (button.isDisabled) return null
-
-                        return (
-                          <MenuItem
-                            key={button.key}
-                            onClick={() => {
-                              button.handleClick()
-                            }}
-                          >
-                            {button.name}
-                          </MenuItem>
-                        )
-                      })}
-                  </Menu>
+                  {buttonList.length === 1 && (
+                    <CustomButton
+                      variant="contained"
+                      onClick={buttonList[0].handleClick}
+                      sx={{
+                        marginRight: isMobile ? '1rem' : 0,
+                        width: isMobile ? '100%' : 'auto',
+                      }}
+                    >
+                      {buttonList[0].name}
+                    </CustomButton>
+                  )}
+                  {buttonList.length === 2 && (
+                    <>
+                      <CustomButton
+                        variant="contained"
+                        onClick={handleOpenBtnList}
+                        sx={{
+                          marginRight: isMobile ? '1rem' : 0,
+                          width: isMobile ? '100%' : 'auto',
+                        }}
+                        endIcon={<ArrowDropDown />}
+                      >
+                        {b3Lang('shoppingList.footer.addSelectedTo')}
+                      </CustomButton>
+                      <Menu
+                        id="basic-menu"
+                        anchorEl={anchorEl}
+                        open={open}
+                        onClose={handleClose}
+                        MenuListProps={{
+                          'aria-labelledby': 'basic-button',
+                        }}
+                      >
+                        {buttonList.length > 1 &&
+                          buttonList.map((button) => (
+                            <MenuItem
+                              key={button.key}
+                              onClick={() => {
+                                button.handleClick()
+                              }}
+                            >
+                              {button.name}
+                            </MenuItem>
+                          ))}
+                      </Menu>
+                    </>
+                  )}
                 </Box>
               )}
             </Box>
