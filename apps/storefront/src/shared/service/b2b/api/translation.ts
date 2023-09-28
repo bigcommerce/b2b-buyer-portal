164ec2e03737b02a0efc149df3096b77e345dd2e
bc-marco <109162781+bc-marco@users.noreply.github.com>
2023-09-28 16:18:29 -0600

    fix: detect origin for translation service (#748) (#751) (#772)

--- apps/storefront/src/shared/service/b2b/api/translation.ts
+++ apps/storefront/src/shared/service/b2b/api/translation.ts
@@ -1,7 +1,7 @@
 import { storeHash } from '@/utils'
 
 import B3Request from '../../request/b3Fetch'
-import { RequestType } from '../../request/base'
+import { B2B_BASIC_URL, RequestType } from '../../request/base'
 
 interface GetTranslationParams {
   channelId: number
@@ -13,9 +13,11 @@ interface GetTranslationResponse {
 
 const { VITE_TRANSLATION_SERVICE_URL } = import.meta.env
 
+const BASE_URL = VITE_TRANSLATION_SERVICE_URL || B2B_BASIC_URL
+
 const getTranslation = ({ channelId, page }: GetTranslationParams) =>
   B3Request.get(
-    `${VITE_TRANSLATION_SERVICE_URL}/storefront/translation/${storeHash}/${channelId}/${page}`,
+    `${BASE_URL}/storefront/translation/${storeHash}/${channelId}/${page}`,
     RequestType.TranslationService
   ) as Promise<GetTranslationResponse>
 
