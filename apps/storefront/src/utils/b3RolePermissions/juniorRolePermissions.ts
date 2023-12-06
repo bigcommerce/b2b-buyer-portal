e8751d3fe931348d3ef57f227a06f826123f246d
BrianJiang2021 <80307788+BrianJiang2021@users.noreply.github.com>
2023-12-06 10:42:53 +0800

    feat: junior hide cart bun1741 (#862)
    * feat: junior hide cart BUN1741
    https://bigc-b2b.atlassian.net/browse/BUN-1741
    
    * feat: junior hide cart bun1741

--- apps/storefront/src/utils/b3RolePermissions/juniorRolePermissions.ts
+++ apps/storefront/src/utils/b3RolePermissions/juniorRolePermissions.ts
@@ -1,15 +1,15 @@
 import globalB3 from '@b3/global-b3'
 
-const removeCartPermissions = (role: number | string) => {
-  if (+role !== 2) return
+const setCartPermissions = (role: number | string) => {
+  if (+role === 2) return
   const carts = document.querySelectorAll(globalB3['dom.cartElement'])
 
   // remove cart Entrance
   if (carts.length > 0) {
     carts.forEach((cart: any) => {
-      cart.style.display = 'none'
+      cart.style.display = ''
     })
   }
 }
 
-export default removeCartPermissions
+export default setCartPermissions
