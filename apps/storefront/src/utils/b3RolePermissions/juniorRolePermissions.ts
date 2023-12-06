19cc660973ad54957c3aa4c9301a66c03f0ff2d3
Brian.Jiang2021 <brian.jiang@bundleb2b.net>
2023-12-06 14:48:19 +0800

    fix: junior css hide cart

--- apps/storefront/src/utils/b3RolePermissions/juniorRolePermissions.ts
+++ apps/storefront/src/utils/b3RolePermissions/juniorRolePermissions.ts
@@ -1,14 +1,8 @@
-import globalB3 from '@b3/global-b3'
-
 const setCartPermissions = (role: number | string) => {
   if (+role === 2) return
-  const carts = document.querySelectorAll(globalB3['dom.cart'])
-
-  // remove cart Entrance
-  if (carts.length > 0) {
-    carts.forEach((cart: any) => {
-      cart.style.display = ''
-    })
+  const style = document.getElementById('b2bPermissions-cartElement-id')
+  if (style) {
+    style.remove()
   }
 }
 
