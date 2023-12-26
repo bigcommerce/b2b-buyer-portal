c94c007eea69f0fd84ae7c9e8f59ec364badce5f
BrianJiang2021 <80307788+BrianJiang2021@users.noreply.github.com>
2023-12-26 16:44:18 +0800

    Revert "fix: junior hide cart optimization"
    This reverts commit 06b8bea11e7e8806ca14adf8ae4a5672f0bb148c.

--- packages/global-b3/index.ts
+++ packages/global-b3/index.ts
@@ -68,7 +68,8 @@ const globalB3 = {
   'dom.setToShoppingListParentEl': '#add-to-cart-wrapper',
   'dom.cartActions.container': '.cart-actions',
   'dom.openB3Checkout': 'checkout-customer-continue',
-  'dom.cart':
+  'dom.cartElement':
+    window?.B3['dom.cart'] ||
     '[href="/cart.php"], #form-action-addToCart, [data-button-type="add-cart"], [data-emthemesmodez-cart-item-add]',
   'dom.productView': '.productView',
   'dom.register': '[href^="/login.php?action=create_account"]',
