3bc085ac26fba96273a7d1039e08b9560bcf7399
Ignacio Catalina <ignacio.catalina@bigcommerce.com>
2024-05-29 10:04:28 +0200

    refactor: soft-type LocalStorage and SessionStorage

--- apps/storefront/src/utils/b3Storage.ts
+++ apps/storefront/src/utils/b3Storage.ts
@@ -1,32 +1,46 @@
-enum StorageType {
-  l = 'localStorage',
-  s = 'sessionStorage',
+// If you change one of this types, bear in mind the stored data
+// will still be the old one, and you will need to handle it.
+interface StorageStore {
+  bcLanguage: string;
+  blockPendingAccountOrderCreation: boolean;
+  blockPendingAccountViewPrice: boolean;
+  cartToQuoteId: string;
+  isAgenting: boolean;
+  isB2BUser: boolean;
+  isShowBlockPendingAccountOrderCreationTip: {
+    cartTip: number;
+    checkoutTip: number;
+  };
+  loginCustomer: string;
+  salesRepCompanyId: string;
+  showInclusiveTaxPrice: boolean;
 }
 
 class MyStorage {
-  storage: Storage;
-
   prefix: string;
 
-  constructor(type: StorageType) {
-    this.storage = type === StorageType.l ? window.localStorage : window.sessionStorage;
+  constructor(private readonly storage: Storage) {
     this.prefix = 'sf-';
   }
 
-  set(key: string, value: any) {
+  set<T extends keyof StorageStore>(key: T, value: StorageStore[T]) {
     const data = JSON.stringify(value);
+
     this.storage.setItem(this.prefix + key, data);
   }
 
-  get(key: string) {
+  get<T extends keyof StorageStore>(key: T): StorageStore[T] | undefined {
     const value = this.storage.getItem(this.prefix + key);
+
     if (value) {
-      return JSON.parse(value);
+      // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+      return JSON.parse(value) as StorageStore[T];
     }
+
     return undefined;
   }
 
-  delete(key: string) {
+  delete(key: keyof StorageStore) {
     this.storage.removeItem(this.prefix + key);
   }
 
@@ -35,7 +49,7 @@ class MyStorage {
   }
 }
 
-const B3LStorage = new MyStorage(StorageType.l);
-const B3SStorage = new MyStorage(StorageType.s);
+const B3LStorage = new MyStorage(window.localStorage);
+const B3SStorage = new MyStorage(window.sessionStorage);
 
 export { B3LStorage, B3SStorage };
