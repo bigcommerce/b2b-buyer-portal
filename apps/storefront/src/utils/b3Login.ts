cbbe2656cb362e8453e27e4d06d5981d51bcb3ed
Brian.Jiang2021 <brian.jiang@bundleb2b.net>
2024-09-03 11:09:05 +0800

    fix: loginLandingLocation login issues

--- apps/storefront/src/utils/b3Login.ts
+++ apps/storefront/src/utils/b3Login.ts
@@ -7,6 +7,12 @@ export const loginJump = (navigate: NavigateFunction, isClearSession = false) =>
     global: { loginLandingLocation, recordOpenHash, setOpenPageFn },
   } = store.getState();
   if (loginLandingLocation === '1' && !recordOpenHash) {
+    if (window.location.href.includes('login.php')) {
+      if (isClearSession) window.sessionStorage.clear();
+      window.location.href = '/';
+      return false;
+    }
+
     navigate('/');
     setOpenPageFn?.({
       isOpen: false,
