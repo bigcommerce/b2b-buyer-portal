c4d640f6ec618b098ca2dd22820c20bba884439f
Victor Campos <victor.campos@bigcommerce.com>
2024-10-31 16:39:20 -0600

    feat: updating gql types

--- apps/storefront/src/types/gql/gql.ts
+++ apps/storefront/src/types/gql/gql.ts
@@ -1,8 +1,8 @@
-/* eslint-disable import/no-extraneous-dependencies */
-/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable */
+import * as types from './graphql';
 import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
 
-const documents: never[] = [];
+const documents = [];
 /**
  * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  *
@@ -21,5 +21,4 @@ export function gql(source: string) {
   return (documents as any)[source] ?? {};
 }
 
-export type DocumentType<TDocumentNode extends DocumentNode<any, any>> =
-  TDocumentNode extends DocumentNode<infer TType, any> ? TType : never;
+export type DocumentType<TDocumentNode extends DocumentNode<any, any>> = TDocumentNode extends DocumentNode<  infer TType,  any>  ? TType  : never;
