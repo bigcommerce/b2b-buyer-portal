a52f27b56908d5e4ee4c82a84efd57e42d14f09d
Micah Thomas <micah.thomas@bigcommerce.com>
2024-11-27 07:58:14 +1100

    fix: remove `/assets` from the CDN_BASE_PATH

--- apps/storefront/src/main.ts
+++ apps/storefront/src/main.ts
@@ -1,11 +1,11 @@
 import { Environment, EnvSpecificConfig } from '@/types';
 
 // TODO: update the following to BC cdn when migration is completed
-const ENVIRONMENT_ASSET_PATH: EnvSpecificConfig<string> = {
+const ENVIRONMENT_CDN_BASE_PATH: EnvSpecificConfig<string> = {
   local: '/',
   integration: 'https://microapp-cdn.gcp.integration.zone/b2b-buyer-portal/',
-  staging: 'https://cdn.bundleb2b.net/b2b/staging/storefront/assets/',
-  production: 'https://cdn.bundleb2b.net/b2b/production/storefront/assets/',
+  staging: 'https://cdn.bundleb2b.net/b2b/staging/storefront/',
+  production: 'https://cdn.bundleb2b.net/b2b/production/storefront/',
 };
 
 window.b2b = {
@@ -15,7 +15,7 @@ window.b2b = {
     // so that the same runtime files can dynamically choose the cdn base url location
     // based on environment it is deployed to
     const environment: Environment = window.B3?.setting?.environment ?? Environment.Production;
-    return `${ENVIRONMENT_ASSET_PATH[environment]}${filename}`;
+    return `${ENVIRONMENT_CDN_BASE_PATH[environment]}${filename}`;
   },
 };
 
