91ff9b6f28e28baed8f5ffc710768b9a4bd7a2d8
Brian.Jiang2021 <brian.jiang@bundleb2b.net>
2024-12-06 14:50:13 +0800

    feat: user management permission

--- apps/storefront/src/components/table/B3PaginationTable.tsx
+++ apps/storefront/src/components/table/B3PaginationTable.tsx
@@ -13,6 +13,7 @@ import isEmpty from 'lodash-es/isEmpty';
 import isEqual from 'lodash-es/isEqual';
 
 import { useMobile } from '@/hooks';
+import { useAppSelector } from '@/store';
 
 import { B3Table, TableColumnItem } from './B3Table';
 
@@ -96,6 +97,11 @@ function PaginationTable(
     offset: 0,
     first: rowsPerPageOptions[0],
   };
+
+  const { selectCompanyHierarchyId } = useAppSelector(
+    ({ company }) => company.companyHierarchyInfo,
+  );
+  const selectCompanyHierarchyIdCache = useRef(selectCompanyHierarchyId);
 
   const cache = useRef(null);
 
@@ -204,10 +210,16 @@ function PaginationTable(
   }, [fetchList, pagination]);
 
   useEffect(() => {
+    const isChangeCompany = +selectCompanyHierarchyIdCache.current !== +selectCompanyHierarchyId;
     if (!isEmpty(searchParams)) {
-      fetchList();
+      if (isChangeCompany) {
+        fetchList(pagination, true);
+        selectCompanyHierarchyIdCache.current = selectCompanyHierarchyId;
+      } else {
+        fetchList();
+      }
     }
-  }, [fetchList, searchParams]);
+  }, [fetchList, searchParams, selectCompanyHierarchyId, pagination]);
 
   useEffect(() => {
     if (getSelectCheckbox) getSelectCheckbox(selectCheckbox);
