ec148d6f4c1ec8b8dc1a186bbba5ea4bcf264dc9
Estefania Ocampo <estefania.ocampo@bigcommerce.com>
2025-01-28 12:12:14 -0600

    fix: return to window origin locally

--- apps/storefront/src/utils/basicConfig.ts
+++ apps/storefront/src/utils/basicConfig.ts
@@ -8,7 +8,7 @@ export const {
 const { VITE_IS_LOCAL_ENVIRONMENT } = import.meta.env;
 
 const generateBcStorefrontAPIBaseUrl = () => {
-  if (VITE_IS_LOCAL_ENVIRONMENT === 'TRUE') return '/bigcommerce';
+  if (VITE_IS_LOCAL_ENVIRONMENT === 'TRUE') return window.origin;
   if (platform === 'bigcommerce') return window.origin;
   if (channelId === 1) return `https://store-${storeHash}.mybigcommerce.com`;
 
