f0a0a6e1548c914059cf41a095303c2f6d46c39b
Ignacio Catalina <ignacio.catalina@bigcommerce.com>
2025-02-14 17:35:15 +0100

    refactor: lift default value setting to the AccountSettings page
    This further separates the fetching of the field information from the
    data. Allowing for a cleaner API separation.

--- apps/storefront/src/pages/AccountSetting/config.ts
+++ apps/storefront/src/pages/AccountSetting/config.ts
@@ -1,14 +1,11 @@
 export interface GetFilterMoreListProps {
-  fieldId: string;
-  rows?: string | number;
   name: string;
+  fieldId: string;
   label: string;
-  required: boolean;
-  default: string;
+  required?: true;
+  disabled?: true;
+  default?: string;
   fieldType: string;
-  xs: number;
-  variant: string;
-  size: string;
 }
 
 export const getAccountSettingsFields = (): GetFilterMoreListProps[] => {
@@ -17,23 +14,15 @@ export const getAccountSettingsFields = (): GetFilterMoreListProps[] => {
       name: 'company',
       fieldId: 'field_company',
       label: 'Company',
-      required: false,
-      default: '',
+      disabled: true,
       fieldType: 'text',
-      xs: 12,
-      variant: 'filled',
-      size: 'small',
     },
     {
       name: 'role',
       fieldId: 'field_role',
       label: 'Role',
-      required: false,
-      default: '',
+      disabled: true,
       fieldType: 'text',
-      xs: 12,
-      variant: 'filled',
-      size: 'small',
     },
   ];
 };
@@ -44,34 +33,19 @@ export const getPasswordModifiedFields = (): GetFilterMoreListProps[] => {
       name: 'currentPassword',
       fieldId: 'field_current_password',
       label: 'Current Password',
-      required: false,
-      default: '',
       fieldType: 'password',
-      xs: 12,
-      variant: 'filled',
-      size: 'small',
     },
     {
       name: 'password',
       fieldId: 'field_password',
       label: 'Password',
-      required: false,
-      default: '',
       fieldType: 'password',
-      xs: 12,
-      variant: 'filled',
-      size: 'small',
     },
     {
       name: 'confirmPassword',
       fieldId: 'field_confirm_password',
       label: 'Confirm Password',
-      required: false,
-      default: '',
       fieldType: 'password',
-      xs: 12,
-      variant: 'filled',
-      size: 'small',
     },
   ];
 };
