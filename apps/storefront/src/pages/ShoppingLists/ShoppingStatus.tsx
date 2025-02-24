5d91986398d6674513380cdeffffecc3b5e045a6
Lee Dunkley <56807262+leeBigCommerce@users.noreply.github.com>
2025-02-25 10:11:29 +1100

    B2B-2150 fix types and simplify getFilterMoreList within ShippingLists (#249)

--- apps/storefront/src/pages/ShoppingLists/ShoppingStatus.tsx
+++ apps/storefront/src/pages/ShoppingLists/ShoppingStatus.tsx
@@ -1,53 +1,47 @@
-import { useB3Lang } from '@b3/lang';
-
 import { B3Tag } from '@/components';
 import { rolePermissionSelector, useAppSelector } from '@/store';
 
-import { getFilterShoppingListStatus } from './config';
+import { useGetFilterShoppingListStatus } from './config';
 
-interface NewStatusProps {
-  label: string;
-  value: string | number;
-  color: string;
-  textColor: string;
-  idLang: string;
-}
+export const useGetStatus = () => {
+  const getFilterShoppingListStatus = useGetFilterShoppingListStatus();
 
-export const getStatus = (submitShoppingListPermission: boolean) => {
-  const statusArr = getFilterShoppingListStatus(submitShoppingListPermission);
+  return (submitShoppingListPermission: boolean) => {
+    const statusArr = getFilterShoppingListStatus(submitShoppingListPermission);
 
-  const newStatus: Array<NewStatusProps> = statusArr.map((item) => {
-    if (Number(item.value) === 0) {
+    const newStatus = statusArr.map((item) => {
+      if (Number(item.value) === 0) {
+        return {
+          color: '#C4DD6C',
+          textColor: 'black',
+          ...item,
+        };
+      }
+
+      if (Number(item.value) === 40) {
+        return {
+          color: '#F4CC46',
+          textColor: 'black',
+          ...item,
+        };
+      }
+
+      if (Number(item.value) === 30) {
+        return {
+          color: '#899193',
+          textColor: '#FFFFFF',
+          ...item,
+        };
+      }
       return {
-        color: '#C4DD6C',
-        textColor: 'black',
-        ...item,
-      };
-    }
-
-    if (Number(item.value) === 40) {
-      return {
-        color: '#F4CC46',
-        textColor: 'black',
-        ...item,
-      };
-    }
-
-    if (Number(item.value) === 30) {
-      return {
-        color: '#899193',
+        color: '#7A6041',
         textColor: '#FFFFFF',
         ...item,
       };
-    }
-    return {
-      color: '#7A6041',
-      textColor: '#FFFFFF',
-      ...item,
-    };
-  });
+    });
 
-  return newStatus;
+    return newStatus;
+  };
 };
 
 interface ShoppingStatusProps {
@@ -56,17 +50,15 @@ interface ShoppingStatusProps {
 
 export function ShoppingStatus({ status }: ShoppingStatusProps) {
   const { submitShoppingListPermission } = useAppSelector(rolePermissionSelector);
+  const getStatus = useGetStatus();
 
-  const b3Lang = useB3Lang();
   const statusList = getStatus(submitShoppingListPermission);
-  const statusItem = statusList.find(
-    (item: NewStatusProps) => Number(item.value) === Number(status),
-  );
+  const statusItem = statusList.find((item) => Number(item.value) === Number(status));
 
   if (statusItem) {
     return (
       <B3Tag color={statusItem.color} textColor={statusItem.textColor}>
-        {b3Lang(statusItem.idLang)}
+        {statusItem.label}
       </B3Tag>
     );
   }
