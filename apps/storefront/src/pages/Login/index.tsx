366c0c1291b558194566ad3c5dd8c0494f45c57f
Ignacio Catalina <ignacio.catalina@bigcommerce.com>
2025-03-14 14:26:17 +0100

    refactor: use plain mui and react-router-dom components
    There's no need to pass callbacks for certain links, we can use native
    components.

--- apps/storefront/src/pages/Login/index.tsx
+++ apps/storefront/src/pages/Login/index.tsx
@@ -373,7 +373,6 @@ export default function Login(props: PageProps) {
                       >
                         <LoginForm
                           loginBtn={loginInfo.loginBtn}
-                          gotoForgotPassword={() => navigate('/forgotPassword')}
                           handleLoginSubmit={handleLoginSubmit}
                           backgroundColor={backgroundColor}
                         />
@@ -389,7 +388,6 @@ export default function Login(props: PageProps) {
                           <LoginPanel
                             createAccountButtonText={loginInfo.createAccountButtonText}
                             widgetBodyText={loginInfo.widgetBodyText}
-                            handleSubmit={() => navigate('/register')}
                           />
                         </Box>
                       )}
