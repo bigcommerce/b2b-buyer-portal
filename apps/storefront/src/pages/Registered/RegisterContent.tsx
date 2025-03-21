41fdc9c0dc5e2a497877c399e34a75623035a5fb
Ignacio Catalina <ignacio.catalina@bigcommerce.com>
2025-03-21 12:14:03 +0100

    refactor(account): B2B-2166 Remove RegisteredStepButton, inline the components in the different pages
    This indirection is causing more harm than good, it is hard to follow
    and requires extra properties to be passed around and check. Removing it
    makes the code simpler to follow.

--- apps/storefront/src/pages/Registered/RegisterContent.tsx
+++ apps/storefront/src/pages/Registered/RegisterContent.tsx
@@ -28,44 +28,30 @@ export const StyledRegisterContent = styled(Box)({
   },
 });
 
-export default function RegisterContent(props: RegisterContentProps) {
-  const { activeStep, handleBack, handleNext, handleFinish } = props;
-
+export default function RegisterContent({
+  activeStep,
+  handleBack,
+  handleNext,
+  handleFinish,
+}: RegisterContentProps) {
   const renderStep = (step: number): ReactNode => {
     switch (step) {
       case 0:
-        return (
-          <RegisteredAccount
-            activeStep={activeStep}
-            handleBack={handleBack}
-            handleNext={handleNext}
-          />
-        );
+        return <RegisteredAccount handleNext={handleNext} />;
 
       case 1:
-        return (
-          <RegisteredDetail
-            activeStep={activeStep}
-            handleBack={handleBack}
-            handleNext={handleNext}
-          />
-        );
+        return <RegisteredDetail handleBack={handleBack} handleNext={handleNext} />;
 
       case 2:
-        return (
-          <RegisterComplete
-            activeStep={activeStep}
-            handleBack={handleBack}
-            handleNext={handleNext}
-          />
-        );
+        return <RegisterComplete handleBack={handleBack} handleNext={handleNext} />;
 
       case 3:
-        return <RegisteredFinish activeStep={activeStep} handleFinish={handleFinish} />;
+        return <RegisteredFinish handleFinish={handleFinish} />;
 
       default:
         return null;
     }
   };
+
   return <StyledRegisterContent component="div">{renderStep(activeStep)}</StyledRegisterContent>;
 }
