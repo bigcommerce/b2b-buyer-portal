c7bbde0c568def4f393bd93422896d408f0c83f2
Ignacio Catalina <ignacio.catalina@bigcommerce.com>
2025-05-23 14:58:02 +0200

    test(common): B2BCAT-36 Improve store mock + share local and global redux store
    - Ensure the global store is restored between test runs to prevent state
      leak.
    
    - Ensure the store setup as part of renderWithProviders is the same one
      consumers take from the global import. This is inline with the
      behaviour in production. The correct solution is to only use the store
      provided through context, but that requires some heavy refactoring in
      some cases.
    
    - Add some test covering these two cases.
    
    - Disable `parallelism: 2`.  This is really of no use in the test task.
      Instead of splitting the load, it is running the same task twice.
    
    - Restrict imports of `lodash` and `msw`, prefer `lodash-es` and
      `test-utils`
    
    - Allow importing dev dependencies from `__mocks__` files.

---
+++ apps/storefront/src/store/index.test.tsx
@@ -0,0 +1,29 @@
+import { buildCompanyStateWith, renderWithProviders } from 'tests/test-utils';
+
+import { store as globalStore, setGlobalCommonState } from '.';
+
+describe('ensure store is restarted on every test', () => {
+  it('should not share state between tests', () => {
+    globalStore.dispatch(setGlobalCommonState({ cartNumber: 123 }));
+    expect(globalStore.getState().global.cartNumber).toBe(123);
+  });
+
+  it('should not share state between tests', () => {
+    expect(globalStore.getState().global.cartNumber).toBe(0);
+  });
+});
+
+it('using preloadedState should seed the global store', () => {
+  const { store } = renderWithProviders(<div />, {
+    preloadedState: {
+      company: buildCompanyStateWith({
+        tokens: {
+          currentCustomerJWT: 'foo',
+        },
+      }),
+    },
+  });
+
+  expect(globalStore.getState().company.tokens.currentCustomerJWT).toBe('foo');
+  expect(globalStore).toBe(store);
+});
