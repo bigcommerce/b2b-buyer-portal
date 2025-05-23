3245a10d564c6955365b8fb9daa83a4e31772b5f
Isaac Chen <isaac.chen@bigcommerce.com>
2025-05-23 17:43:53 +1000

    fix(shopping-lists): B2B-2559 added more builder functions

--- apps/storefront/tests/storeStateBuilders/index.ts
+++ apps/storefront/tests/storeStateBuilders/index.ts
@@ -1,3 +1,4 @@
-export { buildCompanyStateWith } from './companyStateBuilder';
+export { buildCompanyStateWith, buildCustomerWith } from './companyStateBuilder';
 export { buildStoreInfoStateWith } from './storeInfoStateBuilder';
 export { buildB2BFeaturesStateWith } from './b2bFeaturesStateBuilder';
+export { buildGlobalStateWith } from './globalStateBuilder';
