aa9de51d8f114be626e74303f7ec76fab7139185
Isaac Chen <isaac.chen@bigcommerce.com>
2025-05-23 19:52:09 +1000

    fix(shopping-lists): B2B-2559 added a test to cover when there is unexpected error

--- apps/storefront/src/pages/PDP/index.test.tsx
+++ apps/storefront/src/pages/PDP/index.test.tsx
@@ -82,7 +82,7 @@ const buildShoppingListGraphQLResponseNodeWith = builder(() => ({
 }));
 
 describe('when a product without a required variant is added to a shopping list', () => {
-  it('triggers an error alert', async () => {
+  it('triggers an error alert that indicates the product is missing required variant', async () => {
     window.b2b = {
       ...window.b2b,
       utils: {
@@ -404,6 +404,164 @@ describe('when a product with required variants is added to a shopping list', ()
     expect(globalSnackbar.error).not.toHaveBeenCalled();
     expect(globalSnackbar.success).toHaveBeenCalledWith(
       'Products were added to your shopping list',
+      expect.anything(),
+    );
+  });
+});
+
+describe('when an unexpected error occurred during adding a product to shopping list', () => {
+  it('triggers an error alert that indicates something went wrong', async () => {
+    window.b2b = {
+      ...window.b2b,
+      utils: {
+        ...window.b2b?.utils,
+        shoppingList: {
+          ...window.b2b?.utils?.shoppingList,
+          itemFromCurrentPage: [
+            {
+              productId: 123,
+              selectedOptions: { 'attribute[114]': 104 },
+              quantity: 1,
+              productEntityId: 234,
+              optionSelections: { 'attribute[114]': 104 },
+            },
+          ],
+        },
+      },
+    };
+
+    const globalState = buildGlobalStateWith({
+      storeInfo: {
+        platform: 'others',
+      },
+    });
+
+    const superAdminCustomer = buildCustomerWith({
+      role: CustomerRole.SUPER_ADMIN,
+    });
+
+    const companyStateWithSuperAdminUser = buildCompanyStateWith({
+      customer: superAdminCustomer,
+      permissions: [{ code: 'submit_shopping_list_for_approval', permissionLevel: 1 }],
+    });
+
+    const shoppingList = buildShoppingListGraphQLResponseNodeWith({
+      name: 'Test Shopping List 1',
+      status: ShoppingListStatus.Draft,
+    });
+
+    const shoppingListResponse = {
+      totalCount: 1,
+      pageInfo: { hasNextPage: false, hasPreviousPage: false },
+      edges: [
+        {
+          node: shoppingList,
+        },
+      ],
+    };
+
+    const productsResponse = {
+      productsSearch: [
+        {
+          id: 114,
+          name: 'Product with variant',
+          sku: 'test-1',
+          variants: [
+            {
+              variant_id: 179,
+              product_id: 114,
+              sku: 'test-1-1',
+              option_values: [
+                {
+                  id: 103,
+                  label: 'Silver',
+                  option_id: 114,
+                  option_display_name: 'Color',
+                },
+              ],
+            },
+            {
+              variant_id: 180,
+              product_id: 114,
+              sku: 'test-1-2',
+              option_values: [
+                {
+                  id: 104,
+                  label: 'Black',
+                  option_id: 114,
+                  option_display_name: 'Color',
+                },
+              ],
+            },
+          ],
+          imageUrl: '',
+          modifiers: [],
+          options: [
+            {
+              option_id: 114,
+              display_name: 'Color',
+              sort_order: 0,
+              is_required: true,
+            },
+          ],
+          optionsV3: [
+            {
+              id: 114,
+              product_id: 114,
+              name: 'Color1742885038-114',
+              display_name: 'Color',
+              option_values: [
+                {
+                  id: 103,
+                  label: 'Silver',
+                  sort_order: 0,
+                  is_default: false,
+                },
+                {
+                  id: 104,
+                  label: 'Black',
+                  sort_order: 1,
+                  is_default: false,
+                },
+              ],
+              config: [],
+            },
+          ],
+          channelId: [],
+          productUrl: '',
+        },
+      ],
+    };
+
+    vi.spyOn(graphqlModule, 'getB2BShoppingList').mockResolvedValue(shoppingListResponse);
+    vi.spyOn(graphqlModule, 'searchB2BProducts').mockResolvedValue(productsResponse);
+    vi.spyOn(graphqlModule, 'addProductToShoppingList').mockRejectedValue(
+      new Error('Unexpected error'),
+    );
+
+    vi.mock('@/utils/b3Tip', () => ({
+      globalSnackbar: {
+        error: vi.fn(),
+        success: vi.fn(),
+      },
+    }));
+
+    renderWithProviders(<PDP />, {
+      preloadedState: {
+        global: globalState,
+        company: companyStateWithSuperAdminUser,
+      },
+    });
+
+    await waitForElementToBeRemoved(() => screen.queryByText(/loading/i));
+
+    expect(screen.getByText('Add to shopping list')).toBeInTheDocument();
+
+    await userEvent.click(screen.getByText('Test Shopping List 1'));
+    await userEvent.click(screen.getByText('OK'));
+
+    expect(globalSnackbar.error).toHaveBeenCalledWith(
+      'Something went wrong. Please try again.',
       expect.anything(),
     );
   });
