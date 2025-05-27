b360f798b76eebe45a1ba34ac4cd9f18dddce286
Victor Campos <victor.campos@bigcommerce.com>
2025-05-26 23:34:26 -0600

    feat(shopping-lists): B2B-2831 return list without node object

--- apps/storefront/src/components/HeadlessController/index.tsx
+++ apps/storefront/src/components/HeadlessController/index.tsx
@@ -35,6 +35,7 @@ import { getCurrentCustomerInfo } from '@/utils/loginInfo';
 import { endMasquerade, startMasquerade } from '@/utils/masquerade';
 
 import { getSku } from './getSku';
+import { ShoppingListsItemsProps } from '@/pages/ShoppingLists/config';
 
 export interface FormattedQuoteItem
   extends Omit<QuoteItem['node'], 'optionList' | 'calculatedValue' | 'productsSearch'> {
@@ -279,7 +280,7 @@ export default function HeadlessController({ setOpenPage }: HeadlessControllerPr
               ? await getB2BShoppingList()
               : await getBcShoppingList({ channelId });
 
-            return list;
+            return list.map(({ node }: { node: ShoppingListsItemsProps }) => node);
           },
         },
         cart: {
