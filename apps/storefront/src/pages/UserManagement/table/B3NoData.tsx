e0f44e1c0507502f9bdfedac3776654e59e83297
leeBigCommerce <lee.dunkley@bigcommerce.com>
2025-05-27 11:15:09 +1000

    refactor(user-management): B2BCAT-22 remove unused props, inline hardcoded props in UserManagement

--- apps/storefront/src/pages/UserManagement/table/B3NoData.tsx
+++ apps/storefront/src/pages/UserManagement/table/B3NoData.tsx
@@ -3,40 +3,30 @@ import styled from '@emotion/styled';
 import { DataUsageRounded } from '@mui/icons-material';
 
 interface B3NoDataProps {
-  text?: string;
-  backgroundColor?: string;
-  minHeight?: string;
-  isLoading?: boolean;
+  isLoading: boolean;
 }
 
-const NoDataContainer = styled('div')(
-  ({ backgroundColor = '#fff', minHeight = '400px' }: B3NoDataProps) => ({
-    height: '100%',
-    minHeight,
-    backgroundColor,
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    color: '#aaa',
-    fontSize: '18px',
-  }),
-);
+const NoDataContainer = styled('div')(() => ({
+  height: '100%',
+  minHeight: '400px',
+  backgroundColor: '#fff',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  color: '#aaa',
+  fontSize: '18px',
+}));
 
 const NoDataText = styled('span')(() => ({
   marginLeft: '10px',
 }));
 
-export default function B3NoData({
-  text,
-  backgroundColor,
-  minHeight,
-  isLoading = false,
-}: B3NoDataProps) {
+export default function B3NoData({ isLoading }: B3NoDataProps) {
   const b3Lang = useB3Lang();
   return (
-    <NoDataContainer backgroundColor={backgroundColor} minHeight={minHeight}>
+    <NoDataContainer>
       {!isLoading && <DataUsageRounded fontSize="large" />}
-      <NoDataText>{isLoading ? '' : text || b3Lang('global.table.noData')}</NoDataText>
+      <NoDataText>{isLoading ? '' : b3Lang('global.table.noData')}</NoDataText>
     </NoDataContainer>
   );
 }
