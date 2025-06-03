78664eb59910daf9451fb67f4cfc994f1464971d
leeBigCommerce <lee.dunkley@bigcommerce.com>
2025-06-04 09:27:47 +1000

    refactor(user-management): B2BCAT-37 refactor tests to suit new queries and fetching sequence

--- apps/storefront/src/pages/UserManagement/getUsers.ts
+++ apps/storefront/src/pages/UserManagement/getUsers.ts
@@ -10,39 +10,15 @@ const getUsersQl = `
       companyRoleId: $companyRoleId
     ){
       totalCount,
-      pageInfo{
-        hasNextPage,
-        hasPreviousPage,
-      },
       edges{
         node{
           id,
-          createdAt,
-          updatedAt,
           firstName,
           lastName,
           email,
-          phone,
-          bcId,
-          role,
-          uuid,
-          extraFields{
-            fieldName
-            fieldValue
-          }
-          companyRoleId,
           companyRoleName,
-          masqueradingCompanyId,
           companyInfo {
             companyId,
-            companyName,
-            companyAddress,
-            companyCountry,
-            companyState,
-            companyCity,
-            companyZipCode,
-            phoneNumber,
-            bcId,
           },
         }
       }
@@ -54,33 +30,15 @@ export interface UsersResponse {
   data: {
     users: {
       totalCount: number;
-      pageInfo: { hasNextPage: boolean; hasPreviousPage: boolean };
       edges: Array<{
         node: {
           id: string;
-          createdAt: number;
-          updatedAt: number;
           firstName: string;
           lastName: string;
           email: string;
-          phone: string;
-          bcId: number;
-          role: number;
-          uuid: string | null;
-          extraFields: { fieldName: string; fieldValue: string }[];
-          companyRoleId: number;
           companyRoleName: string;
-          masqueradingCompanyId: string | null;
           companyInfo: {
             companyId: string;
-            companyName: string;
-            companyAddress: string;
-            companyCountry: string;
-            companyState: string;
-            companyCity: string;
-            companyZipCode: string;
-            phoneNumber: string;
-            bcId: string;
           };
         };
       }>;
