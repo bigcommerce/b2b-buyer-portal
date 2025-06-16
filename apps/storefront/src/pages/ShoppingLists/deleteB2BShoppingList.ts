6f9433fef68559f78a96c6d3b5295a5a3b469adc
leeBigCommerce <lee.dunkley@bigcommerce.com>
2025-06-16 17:22:56 +1000

    refactor(shopping-lists): B2BCAT-50 fix graphql types within deleteB2BShoppingList

--- apps/storefront/src/pages/ShoppingLists/deleteB2BShoppingList.ts
+++ apps/storefront/src/pages/ShoppingLists/deleteB2BShoppingList.ts
@@ -1,12 +1,23 @@
 import B3Request from '@/shared/service/request/b3Fetch';
 
-const deleteShoppingList = (id: number) => `mutation DeleteShoppingList {
-  shoppingListsDelete(id: ${id}) {
-    message
+const deleteShoppingList = `
+  mutation DeleteShoppingList($id: Int!) {
+    shoppingListsDelete(id: $id) {
+      message
+    }
   }
-}`;
+`;
 
-export const deleteB2BShoppingList = (id: number) =>
-  B3Request.graphqlB2B({
-    query: deleteShoppingList(id),
+interface DeleteShoppingListResponse {
+  data: {
+    shoppingListsDelete: {
+      message: string;
+    };
+  };
+}
+
+export const deleteB2BShoppingList = (id: number | string) =>
+  B3Request.graphqlB2B<DeleteShoppingListResponse>({
+    query: deleteShoppingList,
+    variables: { id: Number(id) },
   });
