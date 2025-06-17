88c6db791bb4acc51904e6817c29b0fc8f0fe785
leeBigCommerce <lee.dunkley@bigcommerce.com>
2025-06-17 13:53:06 +1000

    refactor(shopping-lists): B2BCAT-50 fix graphql types within createB2BShoppingList

--- apps/storefront/src/pages/ShoppingLists/createB2BShoppingList.ts
+++ apps/storefront/src/pages/ShoppingLists/createB2BShoppingList.ts
@@ -1,64 +1,35 @@
 import B3Request from '@/shared/service/request/b3Fetch';
 
-interface ShoppingListParams {
-  id: string | number;
-  sampleShoppingListId: string | number;
+const createShoppingList = `
+  mutation CreateShoppingList ($shoppingListData: ShoppingListsInputType!){
+    shoppingListsCreate (
+      shoppingListData: $shoppingListData
+    ) {
+      shoppingList {
+        id,
+      }
+    }
+  }
+`;
+
+type CreateShoppingListResponse = {
+  data: {
+    shoppingListsCreate: {
+      shoppingList: {
+        id: string;
+      };
+    };
+  };
+};
+
+interface CreateShoppingListVariables {
   name: string;
   description: string;
   status: number;
-  channelId: number;
-  companyId: number;
 }
 
-const getShoppingListInfo = `shoppingList {
-  id,
-  name,
-  description,
-  status,
-  approvedFlag,
-  customerInfo{
-    firstName,
-    lastName,
-    userId,
-    email,
-  },
-  isOwner,
-  grandTotal,
-  totalDiscount,
-  totalTax,
-  isShowGrandTotal,
-  companyInfo {
-    companyId,
-    companyName,
-    companyAddress,
-    companyCountry,
-    companyState,
-    companyCity,
-    companyZipCode,
-    phoneNumber,
-    bcId,
-  },
-}`;
-
-const createShoppingList = (
-  fn: string,
-) => `mutation CreateShoppingList ($shoppingListData: ShoppingListsInputType!){
-  ${fn}(
-    shoppingListData: $shoppingListData
-  ) {
-    ${getShoppingListInfo}
-  }
-}`;
-
-export const createB2BShoppingList = (data: Partial<ShoppingListParams>) =>
-  B3Request.graphqlB2B({
-    query: createShoppingList('shoppingListsCreate'),
-    variables: {
-      shoppingListData: {
-        companyId: data.companyId,
-        name: data.name,
-        description: data.description,
-        status: data.status,
-      },
-    },
+export const createB2BShoppingList = (data: CreateShoppingListVariables) =>
+  B3Request.graphqlB2B<CreateShoppingListResponse>({
+    query: createShoppingList,
+    variables: { shoppingListData: data },
   });
