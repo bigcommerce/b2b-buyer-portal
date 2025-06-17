b802c14122b6318279e794af5cc0dfc164aece64
Victor Campos <victor.campos@bigcommerce.com>
2025-06-17 15:02:02 -0600

    feat(catalyst): B2B-2626 adding styles for catalyst notifications

--- apps/storefront/src/pages/PDP/useAddedToShoppingListAlert.tsx
+++ apps/storefront/src/pages/PDP/useAddedToShoppingListAlert.tsx
@@ -1,8 +1,32 @@
+import { ReactNode } from 'react';
 import { useB3Lang } from '@b3/lang';
 import { Box, Button } from '@mui/material';
 
 import { useAppSelector } from '@/store';
-import { globalSnackbar } from '@/utils';
+import { globalSnackbar, platform } from '@/utils';
+
+function CatalystButton({
+  goToShoppingDetail,
+  children,
+}: {
+  goToShoppingDetail: () => void | undefined;
+  children: ReactNode;
+}) {
+  return (
+    <Button
+      onClick={() => goToShoppingDetail()}
+      variant="text"
+      sx={{
+        color: '#000',
+        padding: 0,
+        fontWeight: 800,
+        textTransform: 'none',
+      }}
+    >
+      {children}
+    </Button>
+  );
+}
 
 export function useAddedToShoppingListAlert() {
   const b3Lang = useB3Lang();
@@ -28,21 +52,27 @@ export function useAddedToShoppingListAlert() {
         >
           <Box
             sx={{
-              mr: '15px',
+              mr: platform === 'catalyst' ? '52px' : '15px',
             }}
           >
             {b3Lang('pdp.notification.productsAdded')}
           </Box>
-          <Button
-            onClick={() => gotoShoppingDetail(id)}
-            variant="text"
-            sx={{
-              color: '#ffffff',
-              padding: 0,
-            }}
-          >
-            {b3Lang('pdp.notification.viewShoppingList')}
-          </Button>
+          {platform === 'catalyst' ? (
+            <CatalystButton goToShoppingDetail={() => gotoShoppingDetail(id)}>
+              {b3Lang('pdp.notification.viewShoppingList')}
+            </CatalystButton>
+          ) : (
+            <Button
+              onClick={() => gotoShoppingDetail(id)}
+              variant="text"
+              sx={{
+                color: '#ffffff',
+                padding: 0,
+              }}
+            >
+              {b3Lang('pdp.notification.viewShoppingList')}
+            </Button>
+          )}
         </Box>
       ),
       isClose: true,
