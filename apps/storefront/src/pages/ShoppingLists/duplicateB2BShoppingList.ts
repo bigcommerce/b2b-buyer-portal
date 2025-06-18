d4f212e02ccc099bf0bae6536d2f6f8878b6a895
leeBigCommerce <lee.dunkley@bigcommerce.com>
2025-06-18 14:50:40 +1000

    refactor(shopping-lists): B2BCAT-50 fix graphql types within duplicateB2BShoppingList

--- apps/storefront/src/pages/ShoppingLists/duplicateB2BShoppingList.ts
+++ apps/storefront/src/pages/ShoppingLists/duplicateB2BShoppingList.ts
@@ -1,65 +1,37 @@
 import B3Request from '@/shared/service/request/b3Fetch';
 
-interface ShoppingListParams {
-  id: string | number;
+const duplicateShoppingList = `
+  mutation DuplicateB2BShoppingList($sampleShoppingListId: Int!, $shoppingListData: ShoppingListsDuplicateInputType!){
+    shoppingListsDuplicate(sampleShoppingListId: $sampleShoppingListId, shoppingListData: $shoppingListData) {
+      shoppingList {
+        id,
+      }
+    }
+  }
+`;
+
+interface DuplicateB2BShoppingListResponse {
+  shoppingListsDuplicate: {
+    shoppingList: {
+      id: number;
+    };
+  };
+}
+
+interface ShoppingListVariables {
   sampleShoppingListId: string | number;
   name: string;
   description: string;
-  status: number;
-  channelId: number;
-  companyId: number;
 }
 
-const getShoppingListInfo = `shoppingList {
-  id,
-  name,
-  description,
-  status,
-  approvedFlag,
-  customerInfo{
-    firstName,
-    lastName,
-    userId,
-    email,
-  },
-  isOwner,
-  grandTotal,
-  totalDiscount,
-  totalTax,
-  isShowGrandTotal,
-  companyInfo {
-    companyId,
-    companyName,
-    companyAddress,
-    companyCountry,
-    companyState,
-    companyCity,
-    companyZipCode,
-    phoneNumber,
-    bcId,
-  },
-}`;
-
-const duplicateShoppingList = (
-  fn: string,
-) => `mutation DuplicateB2BShoppingList($sampleShoppingListId: Int!, $shoppingListData: ShoppingListsDuplicateInputType!){
-  ${fn}(
-    sampleShoppingListId: $sampleShoppingListId
-    shoppingListData: $shoppingListData
-  ) {
-    ${getShoppingListInfo}
-  }
-}`;
-
-export const duplicateB2BShoppingList = (data: Partial<ShoppingListParams>) =>
-  B3Request.graphqlB2B({
-    query: duplicateShoppingList('shoppingListsDuplicate'),
+export const duplicateB2BShoppingList = (data: ShoppingListVariables) =>
+  B3Request.graphqlB2B<DuplicateB2BShoppingListResponse>({
+    query: duplicateShoppingList,
     variables: {
-      sampleShoppingListId: data?.sampleShoppingListId ? Number(data.sampleShoppingListId) : 1,
+      sampleShoppingListId: Number(data.sampleShoppingListId),
       shoppingListData: {
         name: data.name,
         description: data.description,
-        status: data.status,
       },
     },
   });
