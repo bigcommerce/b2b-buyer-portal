d2688f9034e99944ff512aeeda1b26872c5e9fe6
leeBigCommerce <lee.dunkley@bigcommerce.com>
2025-06-19 10:14:55 +1000

    refactor(shopping-lists): B2BCAT-50 copy duplicateB2CShoppingList into ShoppingLists from b2b service

--- apps/storefront/src/pages/ShoppingLists/AddEditShoppingLists.tsx
+++ apps/storefront/src/pages/ShoppingLists/AddEditShoppingLists.tsx
@@ -4,11 +4,7 @@ import { useB3Lang } from '@b3/lang';
 
 import { B3CustomForm } from '@/components';
 import B3Dialog from '@/components/B3Dialog';
-import {
-  duplicateBcShoppingList,
-  updateB2BShoppingList,
-  updateBcShoppingList,
-} from '@/shared/service/b2b';
+import { updateB2BShoppingList, updateBcShoppingList } from '@/shared/service/b2b';
 import { rolePermissionSelector, useAppSelector } from '@/store';
 import { ShoppingListStatus } from '@/types/shoppingList';
 import { channelId, snackbar } from '@/utils';
@@ -21,6 +17,7 @@ import {
 import { createB2BShoppingList } from './createB2BShoppingList';
 import { createBcShoppingList } from './createBcShoppingList';
 import { duplicateB2BShoppingList } from './duplicateB2BShoppingList';
+import { duplicateBcShoppingList } from './duplicateB2CShoppingList';
 
 interface AddEditUserProps {
   renderList: () => void;
