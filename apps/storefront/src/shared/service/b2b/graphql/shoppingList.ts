734fb3ae14640999eba251e20064dcbadcb50aa3
leeBigCommerce <lee.dunkley@bigcommerce.com>
2025-06-19 15:26:03 +1000

    refactor(shopping-lists): B2BCAT-50 revert unneeded query name change within shoppingList

--- apps/storefront/src/shared/service/b2b/graphql/shoppingList.ts
+++ apps/storefront/src/shared/service/b2b/graphql/shoppingList.ts
@@ -382,7 +382,7 @@ shoppingList {
 
 const updateCustomerShoppingList = (
   fn: string,
-) => `mutation UpdateB2CShoppingList($id: Int!, $shoppingListData: CustomerShoppingListsInputType!){
+) => `mutation($id: Int!, $shoppingListData: CustomerShoppingListsInputType!){
   ${fn}(
     id: $id
     shoppingListData: $shoppingListData
