d27020a47b8d505b17fec8008f9272dc4d3a31fe
Victor Campos <victor.campos@bigcommerce.com>
2025-07-14 14:20:13 -0600

    feat(account): B2BCAT-30 changes according to review

---
+++ apps/storefront/src/pages/AccountSetting/UpgradeBanner.tsx
@@ -0,0 +1,52 @@
+import { useB3Lang } from "@b3/lang"
+import { Box, Alert, Typography } from "@mui/material"
+import { Link } from "react-router-dom"
+
+export const UpgradeBanner = () => {
+    const b3Lang = useB3Lang()
+
+    return (
+        <Box>
+          <Alert
+            severity="info"
+            variant="filled"
+            sx={{
+              width: 'inherit',
+              '& button[title="Close"]': {
+                display: 'block',
+              },
+              mb: '24px',
+              maxWidth: '1450px',
+
+              '& .MuiAlert-icon': {
+                padding: '12px 0',
+              },
+
+              '& .MuiAlert-message': {
+                width: '100%',
+              },
+            }}
+          >
+            <Box display="flex" flexWrap="wrap" justifyContent="space-between" width="100%">
+              <Box>
+                <Typography variant="subtitle1" fontWeight="800">
+                  {b3Lang('accountSettings.registeredToB2b.title')}
+                </Typography>
+                <Typography sx={{ textWrap: 'wrap' }}>
+                  {b3Lang('accountSettings.registeredToB2b.description')}
+                </Typography>
+              </Box>
+              <Typography
+                component={Link}
+                to="/registeredbctob2b"
+                sx={{ textDecoration: 'none', textTransform: 'uppercase' }}
+                fontWeight="bold"
+                color="#fff"
+              >
+                {b3Lang('accountSettings.registeredToB2b.upgrade')}
+              </Typography>
+            </Box>
+          </Alert>
+        </Box>
+    ) 
+}
