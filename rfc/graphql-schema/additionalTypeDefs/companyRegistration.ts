108e89acffe798846f6c18292aa5763886c25a69
Ignacio Catalina <ignacio.catalina@bigcommerce.com>
2025-07-16 17:18:46 +0200

    docs(companies): B2BCAT-8 Add schema for Company Registration

---
+++ rfc/graphql-schema/additionalTypeDefs/companyRegistration.ts
@@ -0,0 +1,69 @@
+export default /* GraphQL */ `
+  enum CompanyStatus {
+    PENDING
+    APPROVED
+    REJECTED
+    INACTIVE
+    DELETED
+  }
+
+  extend type Company {
+    id: ID!
+    status: CompanyStatus!
+  }
+
+  input AddCompanyUsersInput {
+    extraFields: CompanyExtraFieldsInput
+  }
+
+  input AddCompanyFileInput {
+    id: ID!
+  }
+
+  input CompanyExtraFieldsInput {
+    multipleChoices: [MultipleChoiceFormFieldInput!]
+    numbers: [NumberFormFieldInput!]
+    texts: [TextFormFieldInput!]
+    multilineTexts: [MultilineTextFormFieldInput!]
+  }
+
+  input AddCompanyAddressInput {
+    firstName: String!
+    lastName: String!
+    address1: String!
+    address2: String
+    city: String!
+    countryCode: String!
+    stateOrProvince: String
+    phone: String
+    postalCode: String
+    extraFields: CompanyExtraFieldsInput
+  }
+
+  input RegisterCompanyInput {
+    name: String!
+    email: String!
+    phone: String!
+    fileList: [AddCompanyFileInput!]
+    companyUser: AddCompanyUsersInput
+    extraFields: CompanyExtraFieldsInput
+    address: AddCompanyAddressInput!
+  }
+
+  type RegisterCompanyError implements Error {
+    message: String!
+  }
+
+  type RegisterCompanyResult {
+    company: Company
+    errors: [RegisterCompanyError!]!
+  }
+
+  extend type CompanyMutations {
+    registerCompany(input: RegisterCompanyInput!): RegisterCompanyResult!
+  }
+
+  extend type Mutation {
+    company: CompanyMutations!
+  }
+`
