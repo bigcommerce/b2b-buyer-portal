11293e32f9d63f8aee084fde9a0e7e28f2dddb16
Ignacio Catalina <ignacio.catalina@bigcommerce.com>
2025-08-05 17:54:53 +0200

    docs(invoices): B2BCAT-72 Add schemas for Invoices/InvoicePayment

---
+++ rfc/graphql-schema/additionalTypeDefs/byPage/invoices.ts
@@ -0,0 +1,147 @@
+export default /* GraphQL */ `
+  enum InvoiceStatus {
+    OPEN
+    PAID
+    OVERDUE
+    CANCELLED
+  }
+
+  input InvoiceDateRangeFilterInput {
+    from: DateTime
+    to: DateTime
+  }
+
+  input InvoiceFiltersInput {
+    search: String
+    dateRange: InvoiceDateRangeFilterInput
+    status: InvoiceStatus
+  }
+
+  enum InvoicesSortInput {
+    INVOICE_NUMBER_A_TO_Z
+    INVOICE_NUMBER_Z_TO_A
+    ORDER_ID_A_TO_Z
+    ORDER_ID_Z_TO_A
+    CREATED_AT_NEWEST
+    CREATED_AT_OLDEST
+    DUE_DATE_NEWEST
+    DUE_DATE_OLDEST
+    ORIGINAL_BALANCE_HIGHEST
+    ORIGINAL_BALANCE_LOWEST
+    OPEN_BALANCE_HIGHEST
+    OPEN_BALANCE_LOWEST
+  }
+
+  type CreateCartFromInvoiceError implements Error {
+    message: String!
+  }
+
+  type CartCreateResult {
+    cart: Cart
+    errors: [CreateCartFromInvoiceError!]!
+  }
+
+  type GenerateInvoicePdfError implements Error {
+    message: String!
+  }
+
+  type GenerateInvoicePdfResult {
+    url: String
+    errors: [GenerateInvoicePdfError!]!
+  }
+
+  type InvoiceBalances {
+    open: Money!
+    original: Money!
+  }
+
+  type Invoice {
+    id: ID!
+    invoiceNumber: String!
+    createdAt: DateTime!
+    dueDate: DateTime!
+    balance: InvoiceBalances!
+    status: InvoiceStatus!
+    order: Order!
+    company: Company!
+  }
+
+  type InvoiceEdge {
+    node: Invoice!
+    cursor: String!
+  }
+
+  type InvoiceConnection {
+    edges: [InvoiceEdge!]!
+    pageInfo: PageInfo!
+    collectionInfo: CollectionInfo!
+  }
+
+  type ExportInvoicesAsCSVResult {
+    url: String
+    errors: [Error!]!
+  }
+
+  type InvoiceTotals {
+    open: Money!
+    overdue: Money!
+  }
+
+  extend type Company {
+    invoiceTotals: InvoiceTotals!
+    invoicesByIds(invoiceIds: [ID!]!): [Invoice!]!
+    invoices(
+      filters: InvoiceFiltersInput
+      sortBy: InvoicesSortInput
+      before: String
+      after: String
+      first: Int
+      last: Int
+    ): InvoiceConnection!
+  }
+
+  input InvoicePaymentInput {
+    id: ID!
+    # If no amount is specified, the whole invoice amount will be used
+    amountToPay: Float
+  }
+
+  type InvoiceMutations {
+    createCartFromInvoices(invoices: [InvoicePaymentInput!]!): CartCreateResult!
+    generateInvoicePdf(invoiceId: ID!): GenerateInvoicePdfResult!
+    exportAsCSVFromSearch(
+      filters: InvoiceFiltersInput
+      sortBy: InvoicesSortInput
+      before: String
+      after: String
+      first: Int
+      last: Int
+    ): ExportInvoicesAsCSVResult!
+    exportAsCSVByIds(invoicesIds: [ID!]!): ExportInvoicesAsCSVResult!
+  }
+
+  type ReceiptLineSet {
+    id: ID!
+    invoiceNumber: ID!
+    amount: Money!
+  }
+
+  type Receipt {
+    paymentId: ID!
+    createdAt: DateTime!
+    transactionType: String
+    paymentType: String
+    totalAmount: Money!
+    referenceNumber: String
+    receiptLineSet: [ReceiptLineSet!]!
+  }
+
+  extend type Query {
+    invoice(invoiceId: ID!): Invoice!
+    receipt(receiptId: ID!): Receipt!
+  }
+
+  extend type Mutation {
+    invoice: InvoiceMutations!
+  }
+`
