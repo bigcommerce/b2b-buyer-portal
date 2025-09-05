1f04d3174411966e474776c8c705fe093fe07d9d
Micah Thomas <micah.thomas@bigcommerce.com>
2025-09-04 18:25:35 -0700

    fix(common): B2B-3463 remove unintentional newline and fix typo in variable

--- packages/eslint-config-b2b/.eslintrc.js
+++ packages/eslint-config-b2b/.eslintrc.js
@@ -91,7 +91,7 @@ module.exports = {
           '**/*.test.tsx',
           '**/__mocks__/**',
           'vite.config.ts',
-          './generate-translations-csv.js',
+          './generate-translations-csv.ts',
         ],
       },
     ],
