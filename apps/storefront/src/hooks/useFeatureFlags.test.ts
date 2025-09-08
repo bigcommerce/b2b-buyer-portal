89e40f732b3a53756dc7fda3a89e4e9f9d515457
Mitch Lombardi <mitch.lombardi@bigcommerce.com>
2025-09-08 11:59:14 -0400

    feat(common): B2B-3325 Add feature flags support

---
+++ apps/storefront/src/hooks/useFeatureFlags.test.ts
@@ -0,0 +1,20 @@
+import { buildGlobalStateWith } from 'tests/storeStateBuilders';
+import { renderHookWithProviders } from 'tests/utils/hook-test-utils';
+import { it } from 'vitest';
+
+import { useFeatureFlags } from './useFeatureFlags';
+
+it('should return feature flags from global state', () => {
+  const preloadedState = {
+    global: buildGlobalStateWith({
+      featureFlags: {
+        'B2B-3318.move_stock_and_backorder_validation_to_backend': true,
+      },
+    }),
+  };
+
+  const { result } = renderHookWithProviders(() => useFeatureFlags(), { preloadedState });
+  expect(result.result.current).toEqual({
+    'B2B-3318.move_stock_and_backorder_validation_to_backend': true,
+  });
+});
