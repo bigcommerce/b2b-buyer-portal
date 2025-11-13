b51bb1db4575986b60d419a96057663c3a17e69e
Ignacio Catalina <ignacio.catalina@bigcommerce.com>
2025-11-13 17:55:16 +0100

    refactor(common): FE-432 Remove * exports in b3CheckPermissions

--- apps/storefront/src/utils/b3CheckPermissions/index.ts
+++ apps/storefront/src/utils/b3CheckPermissions/index.ts
@@ -1,5 +1,20 @@
-export * from './check';
-export * from './base';
-export * from './juniorRolePermissions';
-export * from './config';
-export * from './b2bPermissionPath';
+export {
+  type PermissionCodesProps,
+  type ValidatePermissionWithComparisonTypeProps,
+  type VerifyLevelPermissionProps,
+  checkEveryPermissionsCode,
+  getPermissionsInfo,
+  validatePermissionWithComparisonType,
+  verifyCreatePermission,
+  verifyLevelPermission,
+  verifySubmitShoppingListSubsidiariesPermission,
+} from './check';
+export {
+  checkPermissionCode,
+  getCorrespondsConfigurationPermission,
+  levelComparison,
+  validateBasePermissionWithComparisonType,
+} from './base';
+export { setCartPermissions } from './juniorRolePermissions';
+export { type B2BPermissionsMapParams, b2bPermissionsMap } from './config';
+export { b2bJumpPath } from './b2bPermissionPath';
