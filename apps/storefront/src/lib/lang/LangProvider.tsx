78faccff7776221b972d09e79bef7c4937b662ea
Matthew Volk <matt.volk@bigcommerce.com>
2026-01-23 10:31:42 -0600

    feat(common): CATALYST-1682 update LangProvider for dynamic locale

--- apps/storefront/src/lib/lang/LangProvider.tsx
+++ apps/storefront/src/lib/lang/LangProvider.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from 'react';
+import { ReactNode, useMemo } from 'react';
 import { IntlProvider } from 'react-intl';
 import { useSelector } from 'react-redux';
 
@@ -17,13 +17,24 @@ interface RootState {
   };
 }
 
+const getLocaleFromURL = (): string => {
+  const pathname = window.location?.pathname ?? '';
+  const pathSegments = pathname.split('/').filter(Boolean);
+  const potentialLocale = pathSegments[0];
+  if (potentialLocale && /^[a-z]{2}(-[A-Z]{2})?$/i.test(potentialLocale)) {
+    return potentialLocale.toLowerCase();
+  }
+  return 'en';
+};
+
 function LangProvider({ children, customText = {} }: LangProviderProps) {
   const translations = useSelector<RootState, Translations>(({ lang }) => lang.translations);
+  const currentLocale = useMemo(() => getLocaleFromURL(), []);
 
   return (
     <IntlProvider
       defaultLocale="en"
-      locale="en"
+      locale={currentLocale}
       messages={{ ...locales.en, ...customText, ...translations }}
     >
       {children}
