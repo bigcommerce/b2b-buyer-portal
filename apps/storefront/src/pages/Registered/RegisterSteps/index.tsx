8cc2437437237d9a1dc025a1d4611d5ce1188b80
Veerabhadra Rao Chikka <veerabhadra.chikka@bigcommerce.com>
2026-03-20 14:55:56 +1100

    refactor(register): B2B-4466 Created new RegisterSteps components to simplify

---
+++ apps/storefront/src/pages/Registered/RegisterSteps/index.tsx
@@ -0,0 +1,30 @@
+import { LoginConfig } from '../../Login/config';
+import RegisterContent from '../RegisterContent';
+import RegisteredStep from '../RegisteredStep';
+
+interface RegisterStepsProps {
+  activeStep: number;
+  backgroundColor: string;
+  handleBack: () => void;
+  handleNext: () => void;
+  handleFinish: (config: LoginConfig) => void;
+}
+
+export function RegisterSteps({
+  activeStep,
+  backgroundColor,
+  handleBack,
+  handleNext,
+  handleFinish,
+}: RegisterStepsProps) {
+  return (
+    <RegisteredStep activeStep={activeStep} backgroundColor={backgroundColor}>
+      <RegisterContent
+        activeStep={activeStep}
+        handleBack={handleBack}
+        handleNext={handleNext}
+        handleFinish={handleFinish}
+      />
+    </RegisteredStep>
+  );
+}
