93229c748a508dc02f0a07876c5a261391aa2046
Gaurav Sobti <gaurav.sobti@bigcommerce.com>
2026-03-30 13:03:45 +1100

    refactor(login): B2B-4564 extract useLoginInfo hook

---
+++ apps/storefront/src/pages/Login/useLoginInfo.ts
@@ -0,0 +1,39 @@
+import { useContext } from 'react';
+
+import { useB3Lang } from '@/lib/lang';
+import { CustomStyleContext } from '@/shared/customStyleButton';
+import { defaultCreateAccountPanel } from '@/shared/customStyleButton/context/config';
+import { GlobalContext } from '@/shared/global';
+
+export const useLoginInfo = () => {
+  const {
+    state: { logo },
+  } = useContext(GlobalContext);
+  const {
+    state: { loginPageButton, loginPageDisplay, loginPageHtml },
+  } = useContext(CustomStyleContext);
+  const b3Lang = useB3Lang();
+
+  const { createAccountButtonText, primaryButtonColor, signInButtonText } = loginPageButton;
+  const { displayStoreLogo, pageTitle } = loginPageDisplay;
+
+  const {
+    bottomHtmlRegionEnabled,
+    bottomHtmlRegionHtml,
+    createAccountPanelHtml,
+    topHtmlRegionEnabled,
+    topHtmlRegionHtml,
+  } = loginPageHtml;
+
+  const loginInfo = {
+    loginTitle: pageTitle || b3Lang('login.button.signIn'),
+    loginBtn: signInButtonText || b3Lang('login.button.signInUppercase'),
+    createAccountButtonText: createAccountButtonText || b3Lang('login.button.createAccount'),
+    btnColor: primaryButtonColor || '',
+    widgetHeadText: topHtmlRegionEnabled ? topHtmlRegionHtml : undefined,
+    widgetBodyText: createAccountPanelHtml || defaultCreateAccountPanel,
+    widgetFooterText: bottomHtmlRegionEnabled ? bottomHtmlRegionHtml : undefined,
+    logo: displayStoreLogo ? logo : undefined,
+  };
+  return loginInfo;
+};
