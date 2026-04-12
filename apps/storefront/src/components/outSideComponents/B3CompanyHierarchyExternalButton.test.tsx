fd23b20be6084015d53d6ff12dee88647b5969d9
bc-gary <gary.kumar@bigcommerce.com>
2026-04-13 08:01:53 +1000

    fix(user-management): B2B-4176 Fix test case build failures

--- apps/storefront/src/components/outSideComponents/B3CompanyHierarchyExternalButton.test.tsx
+++ apps/storefront/src/components/outSideComponents/B3CompanyHierarchyExternalButton.test.tsx
@@ -1,3 +1,4 @@
+import { buildGlobalStateWith } from 'tests/storeStateBuilders';
 import {
   buildCompanyStateWith,
   builder,
@@ -45,7 +46,7 @@ describe('when there is a selected company within the company hierarchy', () => 
     });
 
     renderWithProviders(<B3CompanyHierarchyExternalButton isOpen setOpenPage={vi.fn()} />, {
-      preloadedState: { company: companyState, global: { isPageComplete: true } },
+      preloadedState: { company: companyState, global: buildGlobalStateWith({ isPageComplete: true }) },
     });
 
     expect(await screen.findByText('You are representing')).toBeInTheDocument();
@@ -70,7 +71,7 @@ describe('when the user has "companyHierarchy" permissions and clicks on the com
     const setOpenPage = vi.fn();
 
     renderWithProviders(<B3CompanyHierarchyExternalButton isOpen setOpenPage={setOpenPage} />, {
-      preloadedState: { company: companyState, global: { isPageComplete: true } },
+      preloadedState: { company: companyState, global: buildGlobalStateWith({ isPageComplete: true }) },
     });
 
     await userEvent.click(await screen.findByText('ACME'));
@@ -98,7 +99,7 @@ describe('when the user does not have "companyHierarchy" permission, but does ha
 
       renderWithProviders(
         <B3CompanyHierarchyExternalButton isOpen={false} setOpenPage={setOpenPage} />,
-        { preloadedState: { company: companyState, global: { isPageComplete: true } } },
+        { preloadedState: { company: companyState, global: buildGlobalStateWith({ isPageComplete: true }) } },
       );
 
       await userEvent.click(await screen.findByText('ACME'));
