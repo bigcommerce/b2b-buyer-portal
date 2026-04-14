7d92cf04f2fb3d78fc2c82471060af6a5986d7b9
Akshat Kaushik <akshat.kaushik@bigcommerce.com>
2026-04-14 11:28:59 +1000

    chore(common): B2B-4646 Add B2B checkout dynamic test suite to trigger on Staging Launchbay deployments as non-blocking

--- .infrastructure/regression.tf
+++ .infrastructure/regression.tf
@@ -1,4 +1,4 @@
-# Staging regression suite
+# Staging test suites
 
 module "staging_run_suite_staging_b2b_e2e_suite" {
   source     = "git@github.com:bigcommerce/terraform-services-modules.git//modules/run_regression_suite_from_other_project"
@@ -12,7 +12,19 @@ module "staging_run_suite_staging_b2b_e2e_suite" {
   delay_in_seconds   = 0
 }
 
-# Production regression suite
+module "staging_run_suite_staging_b2b_checkout_suite" {
+  source     = "git@github.com:bigcommerce/terraform-services-modules.git//modules/run_regression_suite_from_other_project"
+  project_id = module.service.id
+  target_ids = [
+    module.service.target_ids["staging-us"]
+  ]
+  other_project_name = "functional-tests"
+  other_target_name  = "staging"
+  profile_name       = "staging_b2b_checkout_suite"
+  delay_in_seconds   = 0
+}
+
+# Production deployment requirements
 
 module "production_require_suite_staging_b2b_e2e_suite" {
   source     = "git@github.com:bigcommerce/terraform-services-modules.git//modules/require_regression_suite_from_another_project"
